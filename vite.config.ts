084a6f76153577aa0f3c585f268b3b3ee22ea8e7
Your Name <you@example.com>
2025-11-22 18:12:53 +0700

    init project

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // Make process.env available in client-side code
-    // This is necessary for the @google/genai SDK's Live API feature.
-    'process.env': process.env
-  }
+  
 })
